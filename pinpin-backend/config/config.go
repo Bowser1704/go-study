8dbfe42b65910983f82c9c3530f0b3a434a6b082
Bowser1704 <yuqlang@163.com>
2019-05-27 20:58:16 +0800

    add login

---
+++ pinpin-backend/config/config.go
@@ -0,0 +1,78 @@
+package config
+
+import (
+	_ "github.com/golang/net/dns/dnsmessage"
+	"strings"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/lexkong/log"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Name	string	//配置文件名字
+}
+
+func Init(cfg string) error {
+	c := Config{
+		Name:cfg,
+	}
+
+	//初始化配置文件
+	if err := c.initConfig(); err != nil {
+		return err
+	}
+
+	//初始化日志包
+	c.initLog()
+
+	//监控配置文件变化并热加载程序
+	c.watchConfig()
+
+	return nil
+}
+
+//定义上面用到的方法
+func (c *Config) initConfig() error {
+	if c.Name != "" {
+		viper.SetConfigFile(c.Name)	//如果指定文件则使用指定的配置文件
+	}else {
+		//viper.AddConfigPath("~/go-work/src/github.com/Bowser1704/go-study/gin-demo/conf")	//添加默认的配置文件环境
+		viper.SetConfigFile("conf/config.yaml")	//默认配置文件名字
+	}
+
+	//viper.SetConfigType("yaml")	//默认文件配置格式
+	viper.AutomaticEnv()	//读取匹配的环境变量
+	viper.SetEnvPrefix("PINPIN")	//读取环境变量的前缀为APISERVER
+	replacer := strings.NewReplacer(".","_")	//替换.为_
+	viper.SetEnvKeyReplacer(replacer)
+	if err := viper.ReadInConfig(); err != nil {	//viper解析配置文件
+		return err
+	}
+
+	return nil
+}
+
+func (c *Config) initLog() {
+	passLagerCfg := log.PassLagerCfg{
+
+		Writers:        viper.GetString("log.writers"),
+		LoggerLevel:    viper.GetString("log.logger_level"),
+		LoggerFile:     viper.GetString("log.logger_file"),
+		LogFormatText:  viper.GetBool("log.log_format_text"),
+		RollingPolicy:  viper.GetString("log.rollingPolicy"),
+		LogRotateDate:  viper.GetInt("log.log_rotate_date"),
+		LogRotateSize:  viper.GetInt("log.log_rotate_size"),
+		LogBackupCount: viper.GetInt("log.log_backup_count"),
+	}
+
+	log.InitWithConfig(&passLagerCfg)
+}
+
+//监控配置变化并且热加载程序
+func (c *Config) watchConfig() {
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		log.Infof("Config file changed: %s", e.Name)
+	})
+}
--- test/login.go
+++ test/login.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"net/http"
+	"net/http/cookiejar"
 	"net/url"
 )
 
@@ -12,19 +13,19 @@ const (
 	//login_url string= "https://baidu.com"
 )
 func get_html(requesturl, username, password string) error {
+	jar, _ := cookiejar.New(nil)
 	//client
 	c := &http.Client{}
-	req1, _ := http.NewRequest("GET", requesturl, nil)
-	req1.Header.Set("User-Agent","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36")
-	res, _ := c.Do(req1)
+	c.Jar = jar
 
-	doc, _ := goquery.NewDocumentFromReader(res.Body)
-	//fmt.Println(reflect.TypeOf(doc))
-	//fmt.Println(doc.Html())
+	res1,_ := c.Get(login_url)
+	doc, _ := goquery.NewDocumentFromReader(res1.Body)
+	defer res1.Body.Close()
 	x := doc.Find("div[id=login]").Find("section[class*=btn]")
 	lt, _ := x.Find("input[name=lt]").Attr("value")
 	execution, _ := x.Find("input[name=execution]").Attr("value")
-	postParam := url.Values{
+	fmt.Println(lt,execution)
+	form := url.Values{
 		"username":	{username},
 		"password":	{password},
 		"lt":		{lt},
@@ -32,14 +33,16 @@ func get_html(requesturl, username, password string) error {
 		"_eventld":	{"submit"},
 		"submit":	{"登录"},
 	}
-
-	res1, err1 := http.PostForm(requesturl, postParam)
-	fmt.Println(res1.Header)
-	fmt.Println(res1.Cookies())
-	fmt.Println(err1)
-	return err1
+	res2, err := c.PostForm(login_url, form)
+	doc1, _ := goquery.NewDocumentFromReader(res2.Body)
+	defer res2.Body.Close()
+	fmt.Println(doc1.Html())
+	fmt.Println(res2.Header)
+	fmt.Println(res2.StatusCode)
+	fmt.Println(err)
+	return err
 }
 
 func main() {
-	get_html(login_url,"2018212576","Yu@14796825550")
+	get_html(login_url,"","")
 }
